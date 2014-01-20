db931d272c04655309ec8ebac74d3417e8864400
kentothemes <kentothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-20 03:38:19 +0000

    Initial upload
    git-svn-id: https://plugins.svn.wordpress.org/kento-team/trunk@841538 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,100 @@
+<?php
+/*
+Plugin Name: Kento Team
+Plugin URI: http://kentothemes.com
+Description: Unlimited Team Members for your company website team page
+Version: 1.0
+Author: KentoThemes
+Author URI: http://kentothemes.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+define('KENTO_TEAM_PLUGIN_PATH', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/' );
+wp_enqueue_style('kento-team-style', KENTO_TEAM_PLUGIN_PATH.'css/style.css');
+
+add_filter('widget_text', 'do_shortcode');
+
+function kento_team_contact_add($profile_fields) {
+
+	// Add new fields
+	$profile_fields['position'] = 'Position';
+	$profile_fields['twitter'] = 'Twitter URL';
+	$profile_fields['facebook'] = 'Facebook URL';
+	$profile_fields['googleplus'] = 'Google+ URL';
+	$profile_fields['linkedin'] = 'Linkedin URL';
+	$profile_fields['youtube'] = 'Youtube URL';
+	$profile_fields['skype'] = 'Skype ID';
+	return $profile_fields;
+}
+add_filter('user_contactmethods', 'kento_team_contact_add');
+
+
+
+
+function kento_team_shortcode($atts, $content = null) {
+
+        $atts = shortcode_atts(
+            array(
+                'id' => '',
+				'style' => '',
+            ), $atts);
+
+
+$ids = explode(',', $atts['id']);
+
+foreach ($ids as $id)
+	{
+	
+	$email = get_the_author_meta( user_email, $id );
+	if($atts['style']==3 OR$atts['style']==4 )
+		{
+			$image = get_avatar( $email, 100 );
+		}
+	else
+		{
+		$image = get_avatar( $email, 512 );
+		}
+	
+	
+	$display_name = get_the_author_meta( display_name  , $id );
+	$position = get_the_author_meta(position,$id);
+	$facebook = get_the_author_meta(facebook,$id);
+	$twitter = get_the_author_meta(twitter,$authorid);
+	$googleplus = get_the_author_meta(googleplus,$authorid);
+	$linkedin = get_the_author_meta(linkedin,$authorid);
+	$youtube = get_the_author_meta(youtube,$authorid);
+	$skype = get_the_author_meta(skype,$authorid);
+
+
+
+
+	$count.= "<div class='single-member' id='style-".$atts['style']."'>";	
+	$count.= 	"<div class='single-member-thumbnail'>".$image."</div>";
+	$count.= 	"<div class='single-member-name'>".$display_name."</div>";
+	$count.= 	"<div class='single-member-position'>".$position."</div>";	
+	$count.= 	"<div class='social-links'>";
+	$count.= 	"<div class='facebook sl'><a href='".$facebook."'></a></div>";
+	$count.= 	"<div class='twitter sl'><a href='".$twitter."'></a></div>";
+	$count.= 	"<div class='googleplus sl'><a href='".$googleplus."'></a></div>";	
+	$count.= 	"<div class='linkedin sl'><a href='".$linkedin."'></a></div>";	
+	$count.= 	"<div class='youtube sl'><a href='".$youtube."'></a></div>";	
+	$count.= 	"<div class='skype sl'><a href='skype:".$skype."?call'></a></div>";		
+	
+	$count.= 	"</div>";
+	
+	
+	$count.= "</div>";
+	
+	}
+
+
+
+return "<div class='kento-team'>".$count."</div>";  
+}
+
+add_shortcode('team', 'kento_team_shortcode');
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,67 @@
+=== Kento Team ===
+Contributors: hasanrang05
+Donate link: 
+Tags: team, team members, team plugin wordpress, members profiles, our teams
+Requires at least: 3.5
+Tested up to: 3.8
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Group or team members for your company.
+
+== Description ==
+
+Super simple plugin for displaying team or company members list on page post or anywhere by using shortcodes 
+
+Live Preview: http://kentothemes.com/demo/kento-team/
+
+
+
+
+
+Plugin Features
+
+* Shortcode System.
+* Unlimited Team Members.
+* Four Style to display Team members.
+* Facebook, Twitter, Google Plus, LinkedIn, YouTube, Skype contact link.
+* Custom Profile Field on user profile. 
+* Team Members Image from Gravatar.
+
+
+
+
+== Installation ==
+
+1. Install as regular WordPress plugin.
+2. Go your Pluings setting via WordPress Dashboard and activate it.
+3. This Plugin doesn't have any setting page
+4. After Activate Use short code [team id="1,4,2" style="4"].
+
+<strong>ShortCodes Details</strong>
+
+Use "id" as author id of your wordpress user id
+Use comma(,) by separating multiple user
+
+<strong>How to get user id ?<strong>
+Please go your WordPress Admin Dashboard and "User" menu on users list then hover mouse on "Edit" link on every username bellow and see the value "user_id=3" get this value and add to "id" value on shortcode
+
+and set shortcode value for "style" 1 to 4 meaning of one to four predefined style.
+Please see the screenshot for style.
+
+
+== Screenshots ==
+
+1. Style 1
+2. Style 2
+3. Style 3
+4. Style 4
+
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
